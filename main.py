3d21d3abc6d3ac2fb20a8dbac7a591eeb075e4aa
SDchao <wentaochens@outlook.com>
2020-07-29 22:05:22 +0800

    Add preview list

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ from windows.main_widget import Main_Widget
 
 app = QApplication(sys.argv)
 
-mainWindow = Main_Window(Main_Widget())
-mainWindow.show()
+main_widget = Main_Widget()
+main_window = Main_Window(main_widget)
+main_window.show()
+
+preview_list = main_widget.preview_list
+img_l = []
+for i in range(1,10):
+    img_l.append("windows/images/Default.png")
+preview_list.insert_item(img_l)
 
 sys.exit(app.exec_())
--- windows/preview_list_widget.py
+++ windows/preview_list_widget.py
@@ -1,5 +1,8 @@
-from PySide2.QtCore import QSize
-from PySide2.QtWidgets import QListView, QListWidget
+from PySide2.QtCore import QSize, Qt
+from PySide2.QtWidgets import QListView, QListWidget, QListWidgetItem
+from PySide2.QtGui import QIcon
+from typing import List
+
 
 class Preview_List_Widget(QListWidget):
     def __init__(self):
@@ -7,4 +10,19 @@ class Preview_List_Widget(QListWidget):
         self.setFlow(QListView.LeftToRight)
         self.setViewMode(QListView.IconMode)
         self.setIconSize(QSize(100, 100))
-    
+        self.setWrapping(False)
+        self.setMaximumHeight(150)
+        self.setSpacing(10)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+    def insert_item(self, img_list: List):
+        """Insert serveral image items into ListView
+
+        Args:
+            byteImgList (List): Image List contains image bytes
+        """
+        for img in img_list:
+            item = QListWidgetItem("Default")
+            item.setIcon(QIcon(img))
+            item.setSizeHint(QSize(100, 100))
+            self.addItem(item)
