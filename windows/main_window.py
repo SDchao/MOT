48f0c4bb820cf60651916c413573d59d8f770454
SDchao <wentaochens@outlook.com>
2020-07-31 09:25:29 +0800

    Add about menu

--- windows/main_window.py
+++ windows/main_window.py
@@ -12,11 +12,18 @@ class Main_Window(QMainWindow):
         # Menu
         self.menu = self.menuBar()
         self.file_menu = self.menu.addMenu("文件")
-        # Exit
+        # File
+        ## Exit
         exit_action = QAction("退出", self)
         exit_action.setShortcut("Ctrl+Q")
         exit_action.triggered.connect(self.exit_app)
         self.file_menu.addAction(exit_action)
+
+        # Help
+        self.help_menu = self.menu.addMenu("帮助")
+        ## About
+        about_action = QAction("关于", self)
+        self.help_menu.addAction(about_action)
 
         # Status
         self.status = self.statusBar()
