39d80b09f583e5d32ba5e0a40eb0597ddec47c26
SDchao <wentaochens@outlook.com>
2020-07-31 21:22:33 +0800

    center window

--- main.py
+++ main.py
@@ -12,8 +12,14 @@ main_window.show()
 
 preview_list = main_widget.preview_list
 img_l = []
-for i in range(1,10):
+for i in range(1, 10):
     img_l.append("windows/images/Default.png")
 preview_list.insert_item(img_l)
+
+# Center Screen
+desktopRect = QApplication.desktop().availableGeometry(main_window)
+center = desktopRect.center()
+main_window.move(center.x() - main_window.width() * 0.5,
+                 center.y() - main_window.height() * 0.5)
 
 sys.exit(app.exec_())
