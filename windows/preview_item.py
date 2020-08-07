4603cd67c1edc539d1203898a25cf2418bc8b85e
SDchao <wentaochens@outlook.com>
2020-08-07 17:07:03 +0800

    Add comment

--- windows/preview_item.py
+++ windows/preview_item.py
@@ -1,12 +1,21 @@
 from PySide2.QtWidgets import QListWidgetItem
-from PySide2.QtCore import QSize
+from PySide2.QtCore import QSize, QPoint
 from PySide2.QtGui import QIcon, QPixmap, QImage
 
 
 class PreviewItem(QListWidgetItem):
     video_path: str
+    map_pos: QPoint
 
-    def __init__(self, name: str, icon: QImage, video_path: str, size: QSize = None):
+    def __init__(self, name: str, icon: QImage, video_path: str, map_pos_str: str, size: QSize = None):
+        """
+
+        @param name: 视频预览的显示名称
+        @param icon: 视频预览的图片
+        @param video_path: 视频路径，可为相对路径
+        @param map_pos_str: 地图坐标，为字符串，x、y以空格相隔
+        @param size: 预览Item的大小
+        """
         show_name = name
         if len(name) > 15:
             show_name = name[:15] + "..."
@@ -15,6 +24,9 @@ class PreviewItem(QListWidgetItem):
         self.setIcon(QIcon(QPixmap.fromImage(icon)))
         self.setToolTip(name)
         self.video_path = video_path
+        map_pos_list = map_pos_str.split(" ")
+        assert len(map_pos_list) == 2
+        self.map_pos = QPoint(int(map_pos_list[0]), int(map_pos_list[1]))
         if size:
             self.setSizeHint(size)
         else:
