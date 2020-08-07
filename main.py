17075d69f45bd68c52f23c126852f4bf6155bbee
SDchao <wentaochens@outlook.com>
2020-08-07 17:07:23 +0800

    set map

--- main.py
+++ main.py
@@ -21,22 +21,24 @@ main_window.show()
 #     img_l.append("windows/images/Default.png")
 # preview_list.insert_item(img_l)
 
-# Center Screen
-desktopRect = QApplication.desktop().availableGeometry(main_window)
-center = desktopRect.center()
-main_window.move(center.x() - main_window.width() * 0.5,
-                 center.y() - main_window.height() * 0.5)
 
 # Test info
 info_path = "data/group1/info.json"
 video_path = "data/group1/videos/"
 info = json.load(open(info_path, "r", encoding="utf8"))
 main_window.setWindowTitle(info["name"])
+main_widget.set_map("data/group1/map.jpg")
 cameras_info = info["cameras"]
 for camera_info in cameras_info:
     for video in camera_info["videos"]:
         video_info = video_operator.get_video_info(video_path + video)
-        item = PreviewItem(video_info.name, video_info.preview_image, video_path + video)
+        item = PreviewItem(video_info.name, video_info.preview_image, video_path + video, camera_info["mapPos"])
         main_widget.preview_list.insert_item(item)
+
+# Center Screen
+desktopRect = QApplication.desktop().availableGeometry(main_window)
+center = desktopRect.center()
+main_window.move(center.x() - main_window.width() * 0.5,
+                 center.y() - main_window.height() * 0.5)
 
 sys.exit(app.exec_())
