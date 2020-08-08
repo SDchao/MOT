497a0d3abf14e514ed9af814bae426dee27e79a2
SDchao <wentaochens@outlook.com>
2020-08-08 09:50:31 +0800

    Add mapPos in videoinfo

--- operators/video_operator.py
+++ operators/video_operator.py
@@ -10,13 +10,17 @@ class VideoInfo(object):
     size: QSize
     name: str
     fps: int
+    map_pos: tuple
 
-    def __init__(self, no_err: bool, img: QImage = None, size: QSize = None, name: str = None, fps: int = None):
+    def __init__(self, no_err: bool, img: QImage = None, size: QSize = None, name: str = None, fps: int = None,
+                 map_pos: tuple = None):
         self.no_err = no_err
         self.preview_image = img
         self.size = size
         self.name = name
         self.fps = fps
+        assert len(map_pos) == 2
+        self.map_pos = map_pos
 
     def __str__(self):
         return f"""status: {self.no_err}
@@ -24,10 +28,11 @@ Size-x: {self.size.width()}
 Size-y: {self.size.height()}
 name: {self.name}
 fps: {self.fps}
+mapPos: {self.map_pos}
         """
 
 
-def get_video_info(video_path: str) -> VideoInfo:
+def get_video_info(video_path: str, map_pos: str = None) -> VideoInfo:
     name = os.path.splitext(os.path.split(video_path)[1])[0]
     cap = cv2.VideoCapture(video_path)
     ret, frame = cap.read()
@@ -38,4 +43,7 @@ def get_video_info(video_path: str) -> VideoInfo:
     cap.release()
     q_image = QImage(frame, size.width(), size.height(), 3 * size.width(),
                      QImage.Format_RGB888).rgbSwapped()
+    if map_pos:
+        pos = tuple(map_pos.split(" "))
+        return VideoInfo(True, q_image, size, name, fps, pos)
     return VideoInfo(True, q_image, size, name, fps)
