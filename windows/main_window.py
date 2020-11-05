90411bb965f9c1a3abd344afb335b4873c3cd40c
SDchao <wentaochens@outlook.com>
2020-11-05 21:25:35 +0800

    Add file select dialog

--- windows/main_window.py
+++ windows/main_window.py
@@ -1,6 +1,6 @@
 import json
 
-from PySide2.QtWidgets import QMainWindow, QAction, QApplication
+from PySide2.QtWidgets import QMainWindow, QAction, QApplication, QFileDialog, QMessageBox
 from PySide2.QtCore import Slot
 import sys
 
@@ -20,6 +20,12 @@ class MainWindow(QMainWindow):
         # Menu
         self.menu = self.menuBar()
         self.file_menu = self.menu.addMenu("文件")
+        # Select data
+        open_action = QAction("打开..", self)
+        open_action.setShortcut("Ctrl+O")
+        open_action.triggered.connect(self.open_data)
+        self.file_menu.addAction(open_action)
+
         # Exit
         exit_action = QAction("退出", self)
         exit_action.setShortcut("Ctrl+Q")
@@ -50,6 +56,24 @@ class MainWindow(QMainWindow):
         self.status = self.statusBar()
         self.status.showMessage("正在加载主界面")
 
+    @Slot()
+    def open_data(self):
+        user_selected_dir = QFileDialog.getExistingDirectory(self, "选择数据文件夹", ".", QFileDialog.ShowDirsOnly)
+        if user_selected_dir:
+            err_text = self.set_data(user_selected_dir)
+            if err_text:
+                self.show_message("无法载入数据")
+                msg_box = QMessageBox()
+                msg_box.setText(f"无法载入数据\n{err_text}")
+                msg_box.setWindowTitle("错误")
+                msg_box.setIcon(QMessageBox.Critical)
+                msg_box.exec_()
+            else:
+                msg_box = QMessageBox()
+                msg_box.setWindowTitle("成功")
+                msg_box.setText("载入成功")
+                msg_box.exec_()
+
     def set_data(self, data_root: str):
 
         self.main_widget.clear_data()
@@ -58,32 +82,43 @@ class MainWindow(QMainWindow):
 
         info_path = data_root + "/info.json"
         video_path = data_root + "/videos/"
-        info = json.load(open(info_path, "r", encoding="utf8"))
-        self.setWindowTitle(info["name"])
-        cameras_info = info["cameras"]
-        map_poses = info["mapPos"]
-        self.main_widget.map_label.set_map(data_root + "/map.jpg", map_poses)
-        # main_widget.set_reid_container(reid_container)
+        try:
+            info = json.load(open(info_path, "r", encoding="utf8"))
+            self.setWindowTitle(info["name"])
+            cameras_info = info["cameras"]
+            map_poses = info["mapPos"]
+            self.main_widget.map_label.set_map(data_root + "/map.jpg", map_poses)
+            # main_widget.set_reid_container(reid_container)
 
-        video_item_dict = {}
+            video_item_dict = {}
 
-        for camera_info in cameras_info:
-            for video in camera_info["videos"]:
-                video_name = video[0]
-                video_index = video[1]
-                video_info = video_operator.get_video_info(video_path + video_name)
-                if not video_info.no_err:
-                    logger.error("Unable to find " + video_name)
-                    continue
-                index = int(camera_info["mapPosIndex"])
-                item = PreviewItem(video_info, map_poses[index])
-                # main_widget.add_video(item)
-                video_item_dict[video_index] = item
+            for camera_info in cameras_info:
+                for video in camera_info["videos"]:
+                    video_name = video[0]
+                    video_index = video[1]
+                    video_info = video_operator.get_video_info(video_path + video_name)
+                    if not video_info.no_err:
+                        logger.error("Unable to find " + video_name)
+                        continue
+                    index = int(camera_info["mapPosIndex"])
+                    item = PreviewItem(video_info, map_poses[index])
+                    # main_widget.add_video(item)
+                    video_item_dict[video_index] = item
 
-        video_index_list = list(video_item_dict.keys())
-        video_index_list.sort()
-        for index in video_index_list:
-            self.main_widget.add_video(video_item_dict[index])
+            video_index_list = list(video_item_dict.keys())
+            video_index_list.sort()
+            for index in video_index_list:
+                self.main_widget.add_video(video_item_dict[index])
+
+            return ""
+        except FileNotFoundError as e:
+            logger.error(f"Cannot load file {e.filename}")
+            self.main_widget.clear_data()
+            return f"无法载入文件{e.filename}"
+        except KeyError as e:
+            logger.error(f"Incorrect info json {info_path}, no key named {e}")
+            self.main_widget.clear_data()
+            return f"错误数据格式\n{info_path}\n无法找到键: {e}"
 
     def center_screen(self):
         desktop_rect = QApplication.primaryScreen().geometry()
